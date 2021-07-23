3a550037a7c4ea4773cbd179a58168cd6fb55f13
Archit Jain <arcyjain2002@gmail.com>
2021-07-23 22:42:17 +0530

    Inital commit

---
+++ public/shopdope1.js
@@ -0,0 +1,39 @@
+// arrow1
+function hoverin1(){
+    document.getElementById("arrow1").innerHTML="Fashion &#8595;"
+}
+function hoverout1(){
+    document.getElementById("arrow1").innerHTML="Fashion &#8593;"
+}
+
+//arrow2
+function hoverin2(){
+    document.getElementById("arrow2").innerHTML="Electronics &#8595;"
+}
+function hoverout2(){
+    document.getElementById("arrow2").innerHTML="Electronics &#8593;"
+}
+
+//arrow3
+function hoverin3(){
+    document.getElementById("arrow3").innerHTML="Home &#8595;"
+}
+function hoverout3(){
+    document.getElementById("arrow3").innerHTML="Home &#8593;"
+}
+
+//arrow4
+function hoverin4(){
+    document.getElementById("arrow4").innerHTML="Appliances &#8595;"
+}
+function hoverout4(){
+    document.getElementById("arrow4").innerHTML="Appliances &#8593;"
+}
+
+//arrow5
+function hoverin5(){
+    document.getElementById("arrow5").innerHTML="Beauty Toys and more &#8595;"
+}
+function hoverout5(){
+    document.getElementById("arrow5").innerHTML="Beauty Toys and more &#8593;"
+}
---
+++ shopdopenode.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+// const home = fs.readFileSync('shopdope.html');
+const path= require('path');
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"view"));
+app.use( express.static( "public" ) );
+
+app.get('/',(req,res)=>{
+    res.render("shopdope");
+})
+
+app.get('/mobiles',(req,res)=>{
+    res.render("mobiles");
+})
+
+const hostname='127.0.0.1';
+const port='80';
+
+app.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
