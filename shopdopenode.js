6784532ac30adfe7e1097019932d2c6a1e132624
archit_jain <85511254+architjain2002@users.noreply.github.com>
2021-10-18 18:36:24 +0530

    Update shopdopenode.js

--- shopdopenode.js
+++ shopdopenode.js
@@ -18,9 +18,9 @@ app.get('/mobiles',(req,res)=>{
     res.render("mobiles");
 })
 
-const hostname='127.0.0.1';
+//const hostname='127.0.0.1';
 const port= process.env.PORT || 3000;
 
-app.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}/`);
 })
